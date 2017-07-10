c14361c94133e30f80bba912eab2ef26e627dec3
Max Orelus <me@maxorelus.com>
2017-07-09 23:21:45 -0400

    Added styles to scss file

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -10,34 +10,32 @@ class Results extends Component {
     let items;
     if (this.props.search.response.hits) {
       items = this.props.search.response.hits.hits;
-    } else { items = []; }
+    } else {
+      items = [];
+    }
 
     return (
       <div className="Results__component">
-        {this.props.search.currentPage !== -1 ? (
-          <div className="Results__component constrained__container">
-            <section>
-              <ResultsHeader />
-            </section>
-            <section className="Results__container">
-              {items.map(item => <ResultItem key={item._id} item={item} />)}
-            </section>
-            <section>
-              <ResultsPagination />
-            </section>
-          </div>
-        ) : (
-          <div style={{textAlign: 'center', display: 'block'; width: '100%'}}>
-            Your search did not match any documents =(
-          </div>
-        )}
+        {this.props.search.currentPage !== -1
+          ? <div className="Results__component constrained__container">
+              <section>
+                <ResultsHeader />
+              </section>
+              <section className="Results__container">
+                {items.map(item => <ResultItem key={item._id} item={item} />)}
+              </section>
+              <section>
+                <ResultsPagination />
+              </section>
+            </div>
+          : <div className="Results__no__results">Your search did not match any documents =(</div>}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  search: state.search,
+  search: state.search
 });
 
 export default connect(mapStateToProps)(Results);
