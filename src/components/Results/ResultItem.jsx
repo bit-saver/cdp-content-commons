768c60737f870e4298f4d7f7ee71836ddfb22668
Scott Gustas <scottius@gmail.com>
2017-10-05 12:43:15 -0400

    Adjust card format, clickable source

--- src/components/Results/ResultItem.jsx
+++ src/components/Results/ResultItem.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
-import Chip from 'material-ui/Chip';
+//import Chip from 'material-ui/Chip';
 import SvgIconFile from 'material-ui/svg-icons/editor/insert-drive-file';
-import SvgIconInbox from 'material-ui/svg-icons/content/inbox';
+//import SvgIconInbox from 'material-ui/svg-icons/content/inbox';
 import moment from 'moment'
 import colors from '../../utils/colors';
 import defaultImage from '../../assets/default_image.png';
@@ -13,21 +13,19 @@ class ResultItem extends Component {
   render() {
     const item = this.props.item;
     const source = item._source;
+    const sourcelink = 'https://' + source.site;
 
     return (
       <Card className="ResultItem__component">
         <a rel="noopener noreferrer" href={source.link} title={source.title} target="_blank">
           <CardMedia
-            overlay={<CardTitle subtitle={(
+            overlay={
               <div className="ResultItem__overlay">
-                <p>{source.site}</p>
-                <Chip backgroundColor={colors.scarlet2} labelColor={colors.white}>
-                  <Avatar color={colors.white} style={{ backgroundColor: colors.scarlet1 }} icon={<SvgIconFile />} />
-                  {source.type}
-                </Chip>
+                  <Avatar backgroundColor={colors.white} color={colors.doveGray} icon={<SvgIconFile />} />
               </div>
-            )} />}
-          >
+            }
+            className="ResultItem__cardMedia"
+            overlayContentStyle={{background: 'transparent'}}>
             <div className="ResultItem__featured" style={{
               backgroundImage: `url(${(() => {
                 const image = source.featured_image;
@@ -52,33 +50,46 @@ class ResultItem extends Component {
               title={source.title}>
               {source.title}
             </a>
-          )}
-          subtitle={moment(source.published).format('MM.DD.YYYY')} />
-        <CardText className="ResultItem__excerpt">{source.excerpt}</CardText>
-        <CardActions>
-          {source.categories && (
-            <div className="ResultItem__categories">
-              {
-                source.categories.map((cat, index) => {
-                  if (index > 1) {
-                    return undefined;
-                  }
-                  return (
-                    <Chip
-                      key={index}
-                      backgroundColor={colors.allPorts2}
-                      labelColor={colors.white}
-                      className="ResultItem__chip"
-                    >
-                      <Avatar color={colors.white} style={{ backgroundColor: colors.allPorts1 }} icon={<SvgIconInbox />} />
-                      {cat.name}
-                    </Chip>
-                  );
-                })
-              }
-            </div>
-          )}
-        </CardActions>
+          )} />
+
+        <CardText
+          className="ResultItem__excerpt"
+          style={{ padding: '0 16px' }}
+          >{source.excerpt}
+        </CardText>
+        <CardText 
+          className="ResultItem__date" 
+          style={{ padding: '16px 16px 4px', fontSize: '12px' }}
+          >{moment(source.published).format('MMMM DD, YYYY')}
+        </CardText>
+        <CardText 
+          className="ResultItem__source"
+          style={{ padding: '0px 16px 4px', fontSize: '12px' }}>
+          <a target="_blank" rel="noopener noreferrer" href={sourcelink}>{source.site}</a>
+        </CardText>
+        {source.categories && (
+          <CardText 
+            className="ResultItem__categories"
+            style={{ padding: '0px 16px 4px', fontSize: '12px' }}>
+            {
+              source.categories.map((cat, index) => {
+                if (index > 2) {
+                  return undefined;
+                }
+                if (source.categories.length - 1 !== index && index < 2) {
+                  cat.name = cat.name + '  ·';
+                }
+                return (
+                  <span
+                    key={index}
+                    className="ResultItem__category">
+                    {cat.name.toLowerCase()}
+                  </span>
+                );
+              })
+            }
+          </CardText>
+        )}
       </Card >
     );
   }
