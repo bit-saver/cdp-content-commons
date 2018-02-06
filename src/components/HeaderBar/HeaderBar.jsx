d5c54b281c31deed934f3c46905d61bf7565ef18
Shawn Mathew <shawn.mathew@agencychief.com>
2018-02-06 12:00:30 -0500

    replace search bar with Semantic components, add responsive styling

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -3,28 +3,12 @@ import AppBar from 'material-ui/AppBar';
 import HeaderBarMenu from './HeaderBarMenu';
 //import './HeaderBar.css';
 
-import { Header, Input, Button, Dropdown, Form } from 'semantic-ui-react';
-
-const contentTypes = [
-  { key: 1, text: 'Article' },
-  { key: 2, text: 'Audio' },
-  { key: 3, text: 'Course' },
-  { key: 4, text: 'Image' },
-  { key: 5, text: 'Publication' },
-  { key: 6, text: 'Quiz' },
-  { key: 7, text: 'Video' }
-];
+import { Header } from 'semantic-ui-react';
 
 class HeaderBar extends Component {
-  state = {};
-
-  handleChange = (e, { value }) => this.setState({ value });
-
   render() {
-    const { value } = this.state;
-
     return (
-      <div style={{ 'marginBottom': '80px', 'borderBottom': '2px solid #000' }}>
+      <div>
         {/*
         <AppBar
           className="HeaderBar__component"
@@ -39,25 +23,6 @@ class HeaderBar extends Component {
           Content Commons
           <Header.Subheader>Welcome to the Content Commons. Here you can discover, find, and reuse public diplomacy content from U.S. Department of State resources.</Header.Subheader>
         </Header>        
-        
-        <div className='search_bar' style={{ 'marginBottom': '30px' }}>          
-          <Form>
-            <Form.Group>
-              <Dropdown         
-                icon='chevron down' 
-                text='Content Types' 
-                selection
-                options={contentTypes} 
-                className='search_dropdown'
-                onChange={this.handleChange}
-                value={value}
-              />
-              <Form.Input placeholder='Search...' className='search_input' />
-              <Form.Button icon='search' type='submit' />
-            </Form.Group>
-          </Form>
-        </div>
-
 
       </div>
     );
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,13 +9,14 @@ import SearchAdvanced from './SearchAdvanced';
 
 import { Form, Dropdown } from 'semantic-ui-react';
 const contentTypes = [
-  { key: 1, text: 'Article' },
-  { key: 2, text: 'Audio' },
-  { key: 3, text: 'Course' },
-  { key: 4, text: 'Image' },
-  { key: 5, text: 'Publication' },
-  { key: 6, text: 'Quiz' },
-  { key: 7, text: 'Video' }
+  { key: 0, text: 'All Content Types', value: 'All Content Types' },
+  { key: 1, text: 'Article', value: 'Article' },
+  { key: 2, text: 'Audio', value: 'Audio' },
+  { key: 3, text: 'Course', value: 'Course' },
+  { key: 4, text: 'Image', value: 'Image' },
+  { key: 5, text: 'Publication', value: 'Publication' },
+  { key: 6, text: 'Quiz', value: 'Quiz' },
+  { key: 7, text: 'Video', value: 'Video' }
 ];
 
 
@@ -25,10 +26,12 @@ class Search extends Component {
     // toggles advanced search
     this.state = {
       open: false,
+      contentType: 'Content Types',      
     };
     this.handleQueryOnChange = this.handleQueryOnChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleAdvancedSearchClick = this.handleAdvancedSearchClick.bind(this);
+    this.handleSearchDropdownSelection = this.handleSearchDropdownSelection.bind(this);
   }
   componentWillMount() {
     this.props.loadLanguages();
@@ -55,24 +58,38 @@ class Search extends Component {
       open: !this.state.open,
     });
   }
+
+  handleSearchDropdownSelection(e, { value }) {
+    this.setState({ contentType: value });
+  }
+
   render() {
+    const contentType = this.state.contentType;
+    
     return (    
       <div>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Dropdown         
-              icon='chevron down' 
-              text='Content Types' 
-              selection
-              options={contentTypes} 
-              className='search_dropdown'
-              onChange={this.handleChange}              
-            />
-            <Form.Input placeholder='Search...' onChange={this.handleQueryOnChange} />
-            <Form.Button icon='search' type='submit' />
-          </Form.Group>
-        </Form>
+        <div className='search_bar'>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Dropdown         
+                icon='chevron down'                 
+                selection
+                options={contentTypes} 
+                className='search_dropdown'               
+                text={contentType}
+                onChange={this.handleSearchDropdownSelection}
+              />
+              <Form.Input 
+                placeholder='Search...' 
+                className='search_input'
+                onChange={this.handleQueryOnChange} 
+              />
+              <Form.Button icon='search' type='submit' />
+            </Form.Group>
+          </Form>
+        </div>
 
+        {/*
         <div className="Search__component">        
           <div className="constrained__container">          
             <form onSubmit={this.handleSubmit}>
@@ -103,7 +120,7 @@ class Search extends Component {
             {this.state.open && <SearchAdvanced />}
           </div>
         </div>
-
+        */}
       </div>
     );
   }
