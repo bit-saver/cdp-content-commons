0393898999fe1bae84f981ba4e0d5092635cd26b
Shawn Mathew <shawn.mathew@agencychief.com>
2018-02-28 14:06:50 -0500

    init filter menu

---
+++ src/components/FilterMenu/FilterMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterMenu extends Component {
+  render() {
+  	return(
+  	  <section>
+  	    <p>FilterMenu</p>
+  	  </section>  	  
+  	);
+  }
+}
+
+export default FilterMenu;
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shape, array, number } from 'prop-types';
 import { connect } from 'react-redux';
+import FilterMenu from '../FilterMenu/FilterMenu';
 import ResultsHeader from './ResultsHeader';
 import ResultItem from './ResultItem';
 import ResultsPagination from './ResultsPagination';
@@ -18,33 +19,37 @@ const Results = ( props ) => {
   if ( !items.length && Object.keys( props.search.response ).length ) {
     isNoResults = true;
   }
-
-  return (
-    <section>
-      { props.search.currentPage !== -1 && (
-        <div>
-          <section>
-            <ResultsHeader />
-          </section>
-          <Grid className="results_wrapper">
-            { items.map( item => (
-              <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 4 } className="card_wrapper" key={ item._id }>
-                <ResultItem key={ item._id } item={ item } />
-              </Grid.Column>
-            ) ) }
-
-            { isNoResults && (
-              <div className="Results__no__results">Sorry, your search did not return any results =(</div>
-            ) }
-          </Grid>
-          <section>
-            <ResultsPagination />
-          </section>
-        </div>
-      ) }
-    </section>
-  );
-};
+    return (
+      <section>
+        { props.search.currentPage !== -1 &&
+          <div>          
+            <FilterMenu />
+            <section>
+              <ResultsHeader />
+            </section>
+            <Grid className='results_wrapper'>   
+              { items.map(item => {
+                  <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 4 } className='card_wrapper' key={ item._id }>
+                    <ResultItem key={ item._id } item={ item } />
+                  </Grid.Column>
+                })
+              }
+              
+              { isNoResults &&
+                <div className="Results__no__results">
+                  Sorry, your search did not return any results =(
+                </div>
+              }
+            </Grid>
+            <section>
+              <ResultsPagination />
+            </section>
+          </div>    
+        }
+      </section>
+    );
+  }
+}
 
 const mapStateToProps = state => ( {
   search: state.search
