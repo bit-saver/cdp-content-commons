a6dd60fc8f754cdba7f3a6ab6ea84c89c722a98e
Scott Gustas <scottius@gmail.com>
2018-03-05 14:07:40 -0500

    Make title clickable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import Title from '../Title';
 
 const HeaderItem = () => (
   <section>
-    <Header as="h1" textAlign="center" className="title">
+    <Header as="h1" textAlign="center">
       <Title />
       <Header.Subheader className="subtitle">
         Welcome to the Content Commons. Here you can discover, find, and reuse public diplomacy content from U.S.
