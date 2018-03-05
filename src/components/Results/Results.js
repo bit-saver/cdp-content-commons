8a92947411c169cdd221bc8ffb79264a20921092
Shawn Mathew <shawn.mathew@agencychief.com>
2018-03-05 09:35:35 -0500

    add return stmt to ResultItems map fn

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -19,37 +19,37 @@ const Results = ( props ) => {
   if ( !items.length && Object.keys( props.search.response ).length ) {
     isNoResults = true;
   }
-    return (
-      <section>
-        { props.search.currentPage !== -1 &&
-          <div>          
-            <ResultsFilterMenu />
-            <section>
-              <ResultsHeader />
-            </section>
-            <Grid className='results_wrapper'>   
-              { items.map(item => {
-                  <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 4 } className='card_wrapper' key={ item._id }>
-                    <ResultItem key={ item._id } item={ item } />
-                  </Grid.Column>
-                })
-              }
-              
-              { isNoResults &&
-                <div className="Results__no__results">
-                  Sorry, your search did not return any results =(
-                </div>
-              }
-            </Grid>
-            <section>
-              <ResultsPagination />
-            </section>
-          </div>    
-        }
-      </section>
-    );
-  }
+  return (
+    <section>
+      { props.search.currentPage !== -1 &&
+        <div>          
+          <ResultsFilterMenu />
+          <section>
+            <ResultsHeader />
+          </section>
+          <Grid className='results_wrapper'>   
+            { items.map(item => {
+                return <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 4 } className='card_wrapper' key={ item._id }>
+                  <ResultItem key={ item._id } item={ item } />
+                </Grid.Column>
+              })
+            }
+            
+            { isNoResults &&
+              <div className="Results__no__results">
+                Sorry, your search did not return any results =(
+              </div>
+            }
+          </Grid>
+          <section>
+            <ResultsPagination />
+          </section>
+        </div>    
+      }
+    </section>
+  );
 }
+
 
 const mapStateToProps = state => ( {
   search: state.search
