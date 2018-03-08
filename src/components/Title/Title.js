b6038efbf4e630168d5915621272e9611a0d8b65
Scott Gustas <scottius@gmail.com>
2018-03-08 12:19:00 -0500

    rebase develop

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Title.css';
 
 const Title = () => (
   <Link to="/" className="title" target="_self">Content Commons</Link>
