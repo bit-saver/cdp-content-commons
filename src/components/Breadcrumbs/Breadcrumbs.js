f849dd89569dc41edd8f92424138812f341b525e
Scott Gustas <scottius@gmail.com>
2018-03-28 09:50:47 -0400

    Minor adjustments

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -8,20 +8,14 @@ const Breadcrumbs = ( props ) => {
   const paths = ( props.location.pathname ).split( '/' ).slice( 1 ).filter( Boolean );
 
   return (
-    <Breadcrumb>
+    <Breadcrumb size="small">
       <Breadcrumb.Section href="/">Content Commons</Breadcrumb.Section>
-      { paths.map( ( pathname, i, arr ) => {
-        const breadcrumbSection = ( arr.length - 1 === i )
-          ? <Breadcrumb.Section active className="pathname">{ pathname }</Breadcrumb.Section>
-          : <Breadcrumb.Section className="pathname">{ pathname }</Breadcrumb.Section>;
-
-        return (
-          <span key={ pathname }>
-            <Breadcrumb.Divider icon="right angle" />
-            { breadcrumbSection }
-          </span>
-        );
-      } ) }
+      { paths.map( ( pathname, i, arr ) => (
+        <span key={ pathname }>
+          <Breadcrumb.Divider icon="right angle" />
+          <Breadcrumb.Section active={ arr.length - 1 === i } className="pathname">{ pathname }</Breadcrumb.Section>
+        </span>
+      ) ) }
     </Breadcrumb>
   );
 };
