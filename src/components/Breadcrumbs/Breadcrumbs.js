88d067926b8b924ff8ac943dc62f9463d9a90e4e
Scott Gustas <scottius@gmail.com>
2018-03-28 10:07:14 -0400

    Adjust key

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -6,16 +6,19 @@ import './Breadcrumbs.css';
 
 const Breadcrumbs = ( props ) => {
   const paths = ( props.location.pathname ).split( '/' ).slice( 1 ).filter( Boolean );
-
+  let pathCounter = 0;
   return (
     <Breadcrumb size="small">
       <Breadcrumb.Section href="/">Content Commons</Breadcrumb.Section>
-      { paths.map( ( pathname, i, arr ) => (
-        <span key={ pathname }>
-          <Breadcrumb.Divider icon="right angle" />
-          <Breadcrumb.Section active={ arr.length - 1 === i } className="pathname">{ pathname }</Breadcrumb.Section>
-        </span>
-      ) ) }
+      { paths.map( ( pathname, i, arr ) => {
+        pathCounter += 1;
+        return (
+          <span key={ pathCounter }>
+            <Breadcrumb.Divider icon="right angle" />
+            <Breadcrumb.Section active={ arr.length - 1 === i } className="pathname">{ pathname }</Breadcrumb.Section>
+          </span>
+        );
+      } ) }
     </Breadcrumb>
   );
 };
