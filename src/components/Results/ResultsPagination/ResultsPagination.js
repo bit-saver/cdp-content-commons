f34f6dcfb44c1f6404fc586ff2aff6cf98d84292
Scott Gustas <scottius@gmail.com>
2018-03-30 12:28:20 -0400

    Pagination styles

--- src/components/Results/ResultsPagination/ResultsPagination.js
+++ src/components/Results/ResultsPagination/ResultsPagination.js
@@ -10,19 +10,25 @@ class ResultsPagination extends Component {
   handlePaginationChange = ( e, { activePage } ) => this.props.targetRequest( activePage );
 
   render() {
-    return (
-      <Pagination
-        className="resultsPagination"
-        nextItem="Next ⟩"
-        prevItem="⟨ Previous"
-        activePage={ this.props.search.currentPage }
-        onPageChange={ this.handlePaginationChange }
-        totalPages={ this.props.search.totalPages }
-        siblingRange="2"
-        firstItem={ null }
-        lastItem={ null }
-      />
-    );
+    if ( this.props.search.totalPages > 1 ) {
+      const nextDisabled = this.props.search.currentPage === this.props.search.totalPages;
+      const prevDisabled = this.props.search.currentPage === 1;
+      return (
+        <section className="resultsPagination">
+          <Pagination
+            nextItem={ { content: 'Next ⟩', disabled: nextDisabled } }
+            prevItem={ { content: '⟨ Previous', disabled: prevDisabled } }
+            activePage={ this.props.search.currentPage }
+            onPageChange={ this.handlePaginationChange }
+            totalPages={ this.props.search.totalPages }
+            siblingRange="2"
+            firstItem={ null }
+            lastItem={ null }
+          />
+        </section>
+      );
+    }
+    return <div />;
   }
 }
 
