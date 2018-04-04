a1d8978f6eda6a995b62445c1941ad7fe5e35c92
Scott Gustas <scottius@gmail.com>
2018-04-04 16:13:04 -0400

    Import about content from markdown file

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+export default {
+  // eslint-disable-next-line max-len
+  ABOUT_URL: 'https://raw.githubusercontent.com/IIP-Design/iip-static-assets/master/Commons/AboutPage.md?token=AF0-goqK0TcmVFeWX7BJKnmoDsg98xpoks5azmAUwA%3D%3D',
+  HELP_URL: ''
+};
