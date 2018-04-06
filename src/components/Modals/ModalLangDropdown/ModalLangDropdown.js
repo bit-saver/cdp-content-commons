18efa6cd29946aca637f972c1029f4de598e23d9
Scott Gustas <scottius@gmail.com>
2018-04-06 16:46:47 -0400

    Implement video modal language dropdown

--- src/components/Modals/ModalLangDropdown/ModalLangDropdown.js
+++ src/components/Modals/ModalLangDropdown/ModalLangDropdown.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { array } from 'prop-types';
+import { array, string, func } from 'prop-types';
 import { Dropdown } from 'semantic-ui-react';
 import './ModalLangDropdown.css';
 
@@ -12,29 +12,41 @@ class ModalLangDropdown extends Component {
     };
 
     this.toggleArrow = this.toggleArrow.bind( this );
+    this.handleChange = this.handleChange.bind( this );
   }
 
   toggleArrow() {
     this.setState( { isOpen: !this.state.isOpen } );
   }
 
+  handleChange( e, { value } ) {
+    this.toggleArrow();
+    this.props.handleLanguageChange( value );
+  }
+
   render() {
     const { languages } = this.props;
-    return (
-      <Dropdown
-        className="modal_languages"
-        defaultValue="English"
-        icon={ this.state.isOpen ? 'chevron up' : 'chevron down' }
-        options={ languages }
-        onClick={ this.toggleArrow }
-        onChange={ this.toggleArrow }
-      />
-    );
+    const { selected } = this.props;
+    if ( languages.length > 1 ) {
+      return (
+        <Dropdown
+          className="modal_languages"
+          defaultValue={ selected }
+          icon={ this.state.isOpen ? 'chevron up' : 'chevron down' }
+          options={ languages }
+          onClick={ this.toggleArrow }
+          onChange={ this.handleChange }
+        />
+      );
+    }
+    return <div>{ selected }</div>;
   }
 }
 
 ModalLangDropdown.propTypes = {
-  languages: array
+  languages: array,
+  selected: string,
+  handleLanguageChange: func
 };
 
 export default ModalLangDropdown;
--- src/components/Modals/Video/VideoModal.js
+++ src/components/Modals/Video/VideoModal.js
@@ -22,10 +22,18 @@ import Social from '../../Video/Social';
 import ShareMore from '../../Video/ShareMore';
 
 class VideoModal extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      item: this.props.item.selectedLanguageUnit,
+      selectedLanguage: this.getLanguage()
+    };
+    this.handleLanguageChange = this.handleLanguageChange.bind( this );
+  }
   getVideoSource() {
-    const { item } = this.props;
-    if ( item && item.selectedLanguageUnit ) {
-      const selected = item.selectedLanguageUnit;
+    const { item } = this.state;
+    if ( item ) {
+      const selected = item;
       if ( selected.source && selected.source[0] && selected.source[0].stream && selected.source[0].stream.url ) {
         return selected.source[0].stream.url;
       }
@@ -35,9 +43,9 @@ class VideoModal extends Component {
   }
 
   getVideoTranscript() {
-    const { item } = this.props;
-    if ( item && item.selectedLanguageUnit ) {
-      const selected = item.selectedLanguageUnit;
+    const { item } = this.state;
+    if ( item ) {
+      const selected = item;
       if ( selected.transcript && selected.transcript.text ) {
         return selected.transcript.text;
       }
@@ -50,6 +58,27 @@ class VideoModal extends Component {
     const { selectedLanguageUnit } = this.props.item;
     if ( !selectedLanguageUnit ) return 'English';
     return selectedLanguageUnit.language.display_name;
+  }
+
+  getAvailableLanguages() {
+    return this.props.item.units.map( item => ( {
+      key: item.language.language_code,
+      value: item.language.display_name,
+      text: item.language.display_name
+    } ) );
+  }
+
+  handleLanguageChange( value ) {
+    if ( value ) {
+      for ( let i = 0; i < this.props.item.units.length; i += 1 ) {
+        if ( this.props.item.units[i].language.display_name === value ) {
+          this.setState( {
+            item: this.props.item.units[i],
+            selectedLanguage: value
+          } );
+        }
+      }
+    }
   }
 
   renderVideoPlayer() {
@@ -69,20 +98,15 @@ class VideoModal extends Component {
   }
 
   render() {
-    const tempLanguages = [
-      { key: 'English', value: 'English', text: 'English' },
-      { key: 'Arabic', value: 'Arabic', text: 'Arabic' },
-      { key: 'French', value: 'French', text: 'French' },
-      { key: 'Russian', value: 'Russian', text: 'Russian' },
-      { key: 'Spanish', value: 'Spanish', text: 'Spanish' }
-    ];
-
-    const { item } = this.props;
-
+    const { item } = this.state;
     return (
       <ModalContent headline={ item.title }>
         <div className="modal_options">
-          <ModalLangDropdown languages={ tempLanguages } />
+          <ModalLangDropdown
+            languages={ this.getAvailableLanguages() }
+            selected={ this.state.selectedLanguage }
+            handleLanguageChange={ this.handleLanguageChange }
+          />
           <div className="modal_options_share">
             <img src={ plusIcon } alt="" />
             <PopupTrigger
@@ -117,9 +141,11 @@ class VideoModal extends Component {
                       title: 'Original',
                       component: (
                         <DownloadVideo
-                          selectedLanguageUnit={ this.props.item.selectedLanguageUnit }
-                          instructions={ `Download the original video file without captions in ${this.getLanguage()}.
-                            This download option is best for uploading this video to web pages.` }
+                          selectedLanguageUnit={ item }
+                          instructions={
+                            `Download the original video file without captions in ${item.language.display_name}.
+                            This download option is best for uploading this video to web pages.`
+                          }
                           burnedInCaptions="no"
                         />
                       )
@@ -128,9 +154,11 @@ class VideoModal extends Component {
                       title: this.renderCaptionTabTitle(),
                       component: (
                         <DownloadVideo
-                          selectedLanguageUnit={ this.props.item.selectedLanguageUnit }
-                          instructions={ `Download this video with open captions in ${this.getLanguage()}.
-                            This download option is best for uploading this video to social media` }
+                          selectedLanguageUnit={ item }
+                          instructions={
+                            `Download this video with open captions in ${item.language.display_name}.
+                            This download option is best for uploading this video to social media.`
+                          }
                           burnedInCaptions="yes"
                         />
                       )
@@ -149,7 +177,12 @@ class VideoModal extends Component {
 
         <ModalContentMeta type={ item.type } dateUpdated={ item.modified } transcript={ this.getVideoTranscript() } />
         <ModalDescription description={ item.description } />
-        <ModalPostMeta author={ item.author } source={ item.sourcelink } site={ item.site } datePublished={ item.published } />
+        <ModalPostMeta
+          author={ item.author }
+          source={ item.sourcelink }
+          site={ item.site }
+          datePublished={ item.published }
+        />
         <ModalPostTags tags={ item.categories } />
       </ModalContent>
     );
