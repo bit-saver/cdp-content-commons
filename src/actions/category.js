4621cfdc440f809cb4477c40c0de7fe24796fde2
Terri Regan <contact@terriregan.com>
2018-05-03 11:01:23 -0400

    Updated category filters

--- src/actions/category.js
+++ src/actions/category.js
@@ -1,11 +1,15 @@
-import { categoryAggRequest, categoryBaseRequest } from '../utils/api';
+import { categoryAggRequest, categoryPrimaryRequest, categoryValueNameRequest } from '../utils/api';
 import { titleCase } from '../utils/helpers';
 import { LOAD_CATEGORIES_PENDING, LOAD_CATEGORIES_FAILED, LOAD_CATEGORIES_SUCCESS, CATEGORY_CHANGE } from './types';
+import sortBy from 'lodash.sortby';
 
 export const categoryUpdate = ( category, checked ) => ( {
   type: CATEGORY_CHANGE,
   payload: category
 } );
+
+const removeDuplicates = ( targetArr, prop ) =>
+  targetArr.filter( ( obj, pos, arr ) => arr.map( mapObj => mapObj[prop] ).indexOf( obj[prop] ) === pos );
 
 export const loadCategories = () => async ( dispatch ) => {
   dispatch( { type: LOAD_CATEGORIES_PENDING } );
@@ -14,23 +18,36 @@ export const loadCategories = () => async ( dispatch ) => {
   let primary;
   try {
     response = await categoryAggRequest();
-    primary = await categoryBaseRequest();
+    primary = await categoryPrimaryRequest();
   } catch ( err ) {
     return dispatch( { type: LOAD_CATEGORIES_FAILED } );
   }
 
+  // get primary categories
   const primaryCategories = primary.hits.hits.map( category => category._source.language.en );
-  const categoryBuckets = response.aggregations.category.buckets;
-  const idBuckets = response.aggregations.id.buckets;
+
+  // get all category ids that have associated content
+  const allIds = [...response.aggregations.id.buckets, ...response.aggregations.unitId.buckets];
+
+  // remove duplicate ids
+  const uniqueIds = removeDuplicates( allIds, 'key' );
+
+  // get associated category name from id
+  const categoryNameValuePairs = await categoryValueNameRequest( uniqueIds );
+
+  // onlu include category if it is a primary
+  const primaryCats = categoryNameValuePairs.hits.hits.filter( category =>
+    primaryCategories.includes( category._source.language.en ) );
+
+  // sort list
+  const sorted = sortBy( primaryCats, [o => o._source.language.en] );
 
   // only display primary categories that have associated content
-  const payload = categoryBuckets
-    .filter( category => primaryCategories.includes( category.key ) )
-    .map( ( category, index ) => ( {
-      key: idBuckets[index].key,
-      display: titleCase( category.key ),
-      count: category.doc_count
-    } ) );
+  const payload = sorted.map( category => ( {
+    key: category._id,
+    display: titleCase( category._source.language.en ),
+    count: category.doc_count
+  } ) );
 
   return dispatch( {
     type: LOAD_CATEGORIES_SUCCESS,
