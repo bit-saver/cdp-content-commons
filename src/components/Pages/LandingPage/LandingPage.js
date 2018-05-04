39d1f65ed7db0ae3dbff6c302badfc8574d8c674
Scott Gustas <scottius@gmail.com>
2018-05-04 09:06:14 -0400

    Change recents label to articles

--- src/components/Pages/LandingPage/LandingPage.js
+++ src/components/Pages/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ import './LandingPage.css';
 const LandingPage = () => (
   <section className="landing">
     <Recents label="Videos" type="video" />
-    <Recents label="Posts" type="post" />
+    <Recents label="Articles" type="post" />
   </section>
 );
 
