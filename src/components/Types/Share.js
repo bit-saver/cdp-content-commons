bd4933934393b9ca10ddea6bf117856ea5b4de3b
Shawn Mathew <shawn.mathew@agencychief.com>
2018-06-27 13:27:43 -0400

    edit Video social media sharing to open in popup

--- src/components/Types/Share.js
+++ src/components/Types/Share.js
@@ -6,16 +6,57 @@ import './Share.css';
 const Share = ( props ) => {
   const { shareLink, contentTitle } = props;
   const facebookURL = `https://www.facebook.com/sharer/sharer.php?u=${shareLink}`;
-  const tweet = `https://twitter.com/home?status=${contentTitle} ${shareLink}`;
+  const tweetURL = `https://twitter.com/home?status=${contentTitle} ${shareLink}`;
+
+  const sharePopup = ( url ) => {
+    const { userAgent } = navigator;
+    const mobile = () => (
+      /\b(iPhone|iP[ao]d)/.test( userAgent )
+      || /\b(iP[ao]d)/.test( userAgent )
+      || /Android/i.test( userAgent )
+      || /Mobile/i.test( userAgent )
+    );
+    const screenX = window.screenX !== undefined ? window.screenX : window.screenLeft;
+    const screenY = window.screenY !== undefined ? window.screenY : window.screenTop;
+    const outerWidth = window.outerWidth !== undefined ? window.outerWidth : document.documentElement.clientWidth;
+    const outerHeight = window.outerHeight !== undefined
+      ? window.outerHeight
+      : document.documentElement.clientHeight - 22;
+    const targetWidth = mobile() ? null : 500;
+    const targetHeight = mobile() ? null : 500;
+    const V = screenX < 0 ? window.screen.width + screenX : screenX;
+    const left = parseInt( V + ( ( outerWidth - targetWidth ) / 2 ), 10 );
+    const top = parseInt( screenY + ( ( outerHeight - targetHeight ) / 2.5 ), 10 );
+    const features = [];
+
+    if ( targetWidth !== null ) { features.push( `width=${targetWidth}` ); }
+    if ( targetHeight !== null ) { features.push( `height=${targetHeight}` ); }
+
+    features.push( `left=${left}`, `top=${top}`, 'scrollbars=1' );
+
+    const newWin = window.open( url, contentTitle, features.join( ',' ) );
+    if ( window.focus ) { newWin.focus(); }
+    return newWin;
+  };
 
   return (
     <div>
       <List horizontal className="share_list">
-        <List.Item as="a" href={ facebookURL } target="_blank" >
+        <List.Item
+          as="a"
+          href={ facebookURL }
+          onClick={ ( e ) => { e.preventDefault(); sharePopup( facebookURL ); } }
+          onKeyPress={ ( e ) => { e.preventDefault(); sharePopup( facebookURL ); } }
+        >
           <List.Icon name="facebook f" size="large" />
           <List.Content>Share on Facebook</List.Content>
         </List.Item>
-        <List.Item as="a" href={ tweet } target="_blank">
+        <List.Item
+          as="a"
+          href={ tweetURL }
+          onClick={ ( e ) => { e.preventDefault(); sharePopup( tweetURL ); } }
+          onKeyPress={ ( e ) => { e.preventDefault(); sharePopup( tweetURL ); } }
+        >
           <List.Icon name="twitter" size="large" />
           <List.Content>Share on Twitter</List.Content>
         </List.Item>
