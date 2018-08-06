b697a6e7a92889de3eace6fffc20966a5fbbc322
Shawn Mathew <shawn.mathew@agencychief.com>
2018-08-06 12:24:43 -0400

    recursively check for inline or src scripts in text content parsed html

--- src/components/Modals/ModalText/ModalText.js
+++ src/components/Modals/ModalText/ModalText.js
@@ -4,8 +4,24 @@ import Parser from 'html-react-parser';
 import './ModalText.css';
 
 class ModalText extends Component {
+  static grabScriptContent( item, scriptsArr ) {
+    let scriptContent;
+    if ( item.dangerouslySetInnerHTML ) {
+      scriptContent = { innerHTML: true, content: item.dangerouslySetInnerHTML.__html };
+    } else if ( item.props.src ) {
+      scriptContent = { src: true, content: item.props.src };
+    }
+
+    const isScriptContentInArray = scriptsArr.some( el => el.content === scriptContent.content );
+    if ( !isScriptContentInArray ) {
+      scriptsArr.push( scriptContent );
+    }
+  }
+
   constructor( props ) {
     super( props );
+    this.traverseParsedHTML = this.traverseParsedHTML.bind( this );
+    this.appendScripts = this.appendScripts.bind( this );
     this.state = {
       parsedHTML: Parser( props.textContent )
     };
@@ -15,20 +31,49 @@ class ModalText extends Component {
     const parsedHTMLScripts = [];
     const { parsedHTML } = this.state;
 
-    parsedHTML
-      .filter( ( el ) => {
-        if ( !el.props ) return false;
-        return el.props.children && el.props.children.type === 'script';
-      } )
-      .forEach( ( el ) => {
-        if ( !parsedHTMLScripts.includes( el.props.children.props.src ) ) {
-          parsedHTMLScripts.push( el.props.children.props.src );
+    parsedHTML.forEach( ( el ) => {
+      if ( !el.props ) return;
+      this.traverseParsedHTML( el, parsedHTMLScripts );
+    } );
+    this.appendScripts( parsedHTMLScripts );
+  }
+
+  traverseParsedHTML( el, scriptsArr ) {
+    Object
+      .values( el )
+      .filter( item => item !== null )
+      .forEach( ( item ) => {
+        // If not an object, most likely a string so return
+        if ( typeof item !== 'object' ) return;
+        // Store JS content and return
+        if ( item.type && ( item.type === 'text/javascript' || item.type === 'script' ) ) {
+          return ModalText.grabScriptContent( item, scriptsArr );
+        }
+        // if 'children' prop is an object & item.type is script
+        // store the JS content
+        if ( item.children && typeof item.children === 'object' ) {
+          if ( item.children.type === 'script' ) {
+            return ModalText.grabScriptContent( item.children, scriptsArr );
+          }
+          // Otherwise, recursively traverse nested props
+          if ( typeof item.children.props === 'object' ) {
+            this.traverseParsedHTML( item.children.props, scriptsArr );
+          }
+        }
+        if ( item.children && Array.isArray( item.children ) ) {
+          item.children.forEach( i => this.traverseParsedHTML( i, scriptsArr ) );
         }
       } );
+  }
 
-    parsedHTMLScripts.forEach( ( s ) => {
+  appendScripts( scriptsArr ) {
+    scriptsArr.forEach( ( s ) => {
       const domScript = document.createElement( 'script' );
-      domScript.src = s;
+      if ( s.innerHTML ) {
+        domScript.innerHTML = s.content;
+      } else if ( s.src ) {
+        domScript.src = s.content;
+      }
       this.contentDiv.appendChild( domScript );
     } );
   }
