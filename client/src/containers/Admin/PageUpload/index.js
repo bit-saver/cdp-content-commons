6ecaaedeaa858f0d66be4f21e2bda2f6b612c7a0
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2018-09-07 07:53:18 -0700

    CDP-816: Base Upload Screen
    Added buttons for each content type with disabled states for all but Video.

--- client/src/containers/Admin/PageUpload/index.js
+++ client/src/containers/Admin/PageUpload/index.js
@@ -8,8 +8,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import audioIcon from 'assets/icons/icon_150px_audio_blue.png';
+import videoIcon from 'assets/icons/icon_150px_video_blue.png';
+import imageIcon from 'assets/icons/icon_150px_images_blue.png';
+import docIcon from 'assets/icons/icon_150px_document_blue.png';
+import eduIcon from 'assets/icons/icon_150px_edu_blue.png';
 import * as actions from './actions';
 import makeSelectPageUpload from './selectors';
+import { Button } from 'semantic-ui-react';
 
 import './PageUpload.css';
 
@@ -22,7 +28,41 @@ class PageUpload extends React.Component {
           <title>Upload Content</title>
           <meta name="description" content="Upload content to the Content Commons" />
         </Helmet>
-        <div>[ PAGE UPLOAD CONTENT ]</div>
+        <div className="upload-content">
+          <h2>Upload content</h2>
+          <div className="contentTypes">
+            <Button icon className="type disabled">
+              <img src={ audioIcon } alt="" />
+              <div className="type--label">
+                Audio
+              </div>
+            </Button>
+            <Button icon className="type">
+              <img src={ videoIcon } alt="" />
+              <div className="type--label">
+                Videos
+              </div>
+            </Button>
+            <Button icon className="type disabled">
+              <img src={ imageIcon } alt="" />
+              <div className="type--label">
+                Images
+              </div>
+            </Button>
+            <Button icon className="type disabled">
+              <img src={ docIcon } alt="" />
+              <div className="type--label">
+                Documents
+              </div>
+            </Button>
+            <Button icon className="type disabled">
+              <img src={ eduIcon } alt="" />
+              <div className="type--label">
+                Teaching Materials
+              </div>
+            </Button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -36,4 +76,3 @@ const mapStateToProps = ( state, props ) => createStructuredSelector( {
 } );
 
 export default connect( mapStateToProps, actions )( PageUpload );
-
