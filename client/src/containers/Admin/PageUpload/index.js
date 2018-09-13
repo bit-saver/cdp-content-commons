a9b89df8d62e708344891633fede16f129040ec8
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2018-09-13 11:14:06 -0700

    CDP-816: Base Upload Screen
    Removed unneeded components (eg markdown).
    Improved accessibility with p tags.
    Fixed about (FAQs) link address.

--- client/src/containers/Admin/PageUpload/index.js
+++ client/src/containers/Admin/PageUpload/index.js
@@ -4,31 +4,23 @@
  *
  */
 import React from 'react';
-// import PropTypes from 'prop-types';
+import Page from 'components/Page';
+import Breadcrumbs from 'components/Breadcrumbs';
+import { Button, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 import audioIcon from 'assets/icons/icon_150px_audio_blue.png';
 import videoIcon from 'assets/icons/icon_150px_video_blue.png';
 import imageIcon from 'assets/icons/icon_150px_images_blue.png';
 import docIcon from 'assets/icons/icon_150px_document_blue.png';
 import eduIcon from 'assets/icons/icon_150px_edu_blue.png';
 
-import Page from 'components/Page';
-import Breadcrumbs from 'components/Breadcrumbs';
-import Error from 'components/Error';
-import config from 'config';
-import withMarkdown from 'hocs/withMarkdown';
-
-import { Button, Header } from 'semantic-ui-react';
-
 import './PageUpload.css';
-import ReactMarkdown from 'react-markdown';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
-const PageUpload = props => (
+const PageUpload = () => (
   <Page title="Upload Content" description="Upload content to the Content Commons">
     <Breadcrumbs />
     <Header as="h2">Upload Content</Header>
-    { props.error ? <Error /> : <ReactMarkdown source={ props.markdown } /> }
     <div className="upload-content">
       <div className="contentTypes">
         <Button icon className="type disabled">
@@ -69,20 +61,16 @@ const PageUpload = props => (
         <li>You have the right to upload.</li>
         <li>Are allowed on the CDP servers.</li>
       </ol>
-      Still have questions?
-      <br />
-      <br />
-      Read our <Link to="about">FAQs</Link>&nbsp;
-      about uploading content.
+      <p>Still have questions?</p>
+      <p>
+        Read our
+        &nbsp;
+        <Link to="/about">FAQs</Link>
+        &nbsp;
+        about uploading content.
+      </p>
     </div>
   </Page>
 );
 
-PageUpload.propTypes = {
-  markdown: PropTypes.string,
-  error: PropTypes.bool
-};
-
-const WrappedComponent = withMarkdown( PageUpload, config.UPLOAD_URL );
-
-export default WrappedComponent;
+export default PageUpload;
