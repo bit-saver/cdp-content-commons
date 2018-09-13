6c443db416b33ec5fa664e771515e733ebb862e7
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2018-09-13 08:33:19 -0700

    CDP-816: Base Upload Screen
    Adjusted responsive margin on contentTypes div.
    Added advisory at the bottom.
    Switched to page component (static page).

--- client/src/containers/Admin/PageUpload/index.js
+++ client/src/containers/Admin/PageUpload/index.js
@@ -5,74 +5,84 @@
  */
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import audioIcon from 'assets/icons/icon_150px_audio_blue.png';
 import videoIcon from 'assets/icons/icon_150px_video_blue.png';
 import imageIcon from 'assets/icons/icon_150px_images_blue.png';
 import docIcon from 'assets/icons/icon_150px_document_blue.png';
 import eduIcon from 'assets/icons/icon_150px_edu_blue.png';
-import * as actions from './actions';
-import makeSelectPageUpload from './selectors';
-import { Button } from 'semantic-ui-react';
+
+import Page from 'components/Page';
+import Breadcrumbs from 'components/Breadcrumbs';
+import Error from 'components/Error';
+import config from 'config';
+import withMarkdown from 'hocs/withMarkdown';
+
+import { Button, Header } from 'semantic-ui-react';
 
 import './PageUpload.css';
+import ReactMarkdown from 'react-markdown';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-/* eslint-disable react/prefer-stateless-function */
-class PageUpload extends React.Component {
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Upload Content</title>
-          <meta name="description" content="Upload content to the Content Commons" />
-        </Helmet>
-        <div className="upload-content">
-          <h2>Upload content</h2>
-          <div className="contentTypes">
-            <Button icon className="type disabled">
-              <img src={ audioIcon } alt="" />
-              <div className="type--label">
-                Audio
-              </div>
-            </Button>
-            <Button icon className="type">
-              <img src={ videoIcon } alt="" />
-              <div className="type--label">
-                Videos
-              </div>
-            </Button>
-            <Button icon className="type disabled">
-              <img src={ imageIcon } alt="" />
-              <div className="type--label">
-                Images
-              </div>
-            </Button>
-            <Button icon className="type disabled">
-              <img src={ docIcon } alt="" />
-              <div className="type--label">
-                Documents
-              </div>
-            </Button>
-            <Button icon className="type disabled">
-              <img src={ eduIcon } alt="" />
-              <div className="type--label">
-                Teaching Materials
-              </div>
-            </Button>
+const PageUpload = props => (
+  <Page title="Upload Content" description="Upload content to the Content Commons">
+    <Breadcrumbs />
+    <Header as="h2">Upload Content</Header>
+    { props.error ? <Error /> : <ReactMarkdown source={ props.markdown } /> }
+    <div className="upload-content">
+      <div className="contentTypes">
+        <Button icon className="type disabled">
+          <img src={ audioIcon } alt="" />
+          <div className="type--label">
+            Audio
           </div>
-        </div>
+        </Button>
+        <Button icon className="type">
+          <img src={ videoIcon } alt="" />
+          <div className="type--label">
+            Videos
+          </div>
+        </Button>
+        <Button icon className="type disabled">
+          <img src={ imageIcon } alt="" />
+          <div className="type--label">
+            Images
+          </div>
+        </Button>
+        <Button icon className="type disabled">
+          <img src={ docIcon } alt="" />
+          <div className="type--label">
+            Documents
+          </div>
+        </Button>
+        <Button icon className="type disabled">
+          <img src={ eduIcon } alt="" />
+          <div className="type--label">
+            Teaching Materials
+          </div>
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+    <div className="upload-advisory">
+      <h3>Only upload files that:</h3>
+      <ol>
+        <li>You have the right to upload.</li>
+        <li>Are allowed on the CDP servers.</li>
+      </ol>
+      Still have questions?
+      <br />
+      <br />
+      Read our <Link to="about">FAQs</Link>&nbsp;
+      about uploading content.
+    </div>
+  </Page>
+);
 
 PageUpload.propTypes = {
+  markdown: PropTypes.string,
+  error: PropTypes.bool
 };
 
-const mapStateToProps = ( state, props ) => createStructuredSelector( {
-  pageupload: makeSelectPageUpload()
-} );
+const WrappedComponent = withMarkdown( PageUpload, config.UPLOAD_URL );
 
-export default connect( mapStateToProps, actions )( PageUpload );
+export default WrappedComponent;
