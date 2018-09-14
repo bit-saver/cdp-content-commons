0ed4b07c37367a1ee14879739fe1c2d30246b4a1
Terri Regan <contact@terriregan.com>
2018-09-14 15:49:37 -0400

    Added basic language detection

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,10 +3,22 @@ import { withRouter } from 'react-router-dom';
 import { func, shape, string, object } from 'prop-types';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-import { Form } from 'semantic-ui-react';
+import { Form, Input } from 'semantic-ui-react';
 import './Search.css';
 
+// Text language detect - move to v2
+import axios from 'axios';
+import { languages, getDirection } from '../../utils/language';
+
 class Search extends Component {
+  constructor( props ) {
+    super( props );
+    this.URL = `${process.env.REACT_APP_PUBLIC_API}/v1/util/language`;
+    this.state = {
+      direction: 'left'
+    };
+  }
+
   componentDidMount() {
     // Clear session if on home page
     if ( this.props.location.pathname === '/' ) {
@@ -16,8 +28,30 @@ class Search extends Component {
     }
   }
 
-  handleQueryOnChange = ( e ) => {
-    this.props.updateSearchQuery( e.target.value );
+
+  async fetchTextLanguage( text ) {
+    const result = await axios.post( this.URL, { text } );
+    const data = ( result && result.data ) ? result.data : null;
+    if ( data && data[0] && data[0].language ) {
+      const { language } = data[0];
+      this.setState( {
+        direction: getDirection( language )
+      } );
+      return ( languages[language] ) ? languages[language] : null;
+    }
+    return null;
+  }
+
+  handleQueryOnChange = async ( e ) => {
+    const text = e.target.value;
+    this.props.updateSearchQuery( text );
+    this.fetchTextLanguage( text )
+      .then( ( language ) => {
+        if ( language ) {
+          this.props.languageUpdate( language );
+        }
+      } )
+      .catch( err => console.log( err ) );
   };
 
   handleSubmit = ( e ) => {
@@ -28,18 +62,35 @@ class Search extends Component {
   };
 
   render() {
+    let inputProps = {};
+    if ( this.state.direction === 'left' ) {
+      inputProps = { className: 'search_input' };
+    } else {
+      inputProps = { className: 'search_input right', iconPosition: 'left' };
+    }
+
     return (
       <section className="search_bar">
         <Form onSubmit={ this.handleSubmit }>
-          <Form.Group>
+          <Input
+            onChange={ this.handleQueryOnChange }
+            value={ this.props.search.query ? this.props.search.query : '' }
+            size="large"
+            icon="search"
+            placeholder="Type in keywords to search our content"
+            { ...inputProps }
+          />
+          { /* <Form.Group>
             <Form.Input
               placeholder="Type in keywords to search our content"
               className="search_input"
+             // onBlur={ this.handleOnBlur }
               onChange={ this.handleQueryOnChange }
               value={ this.props.search.query ? this.props.search.query : '' }
+              style={ { textAlign: this.state.textAlign } }
             />
             <Form.Button icon="search" type="submit" />
-          </Form.Group>
+          </Form.Group> */ }
         </Form>
       </section>
     );
@@ -54,6 +105,7 @@ Search.propTypes = {
   updateSearchQuery: func,
   createRequest: func,
   clearFilters: func,
+  languageUpdate: func,
   history: object,
   location: object,
   search: shape( {
@@ -63,3 +115,4 @@ Search.propTypes = {
 
 // wrap component in withRouter to get access to history
 export default withRouter( connect( mapStateToProps, actions )( Search ) );
+
