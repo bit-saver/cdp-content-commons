9a26d01c0c60498171b315689486a81546d3aaf0
Terri Regan <contact@terriregan.com>
2018-10-22 15:25:32 -0400

    Port google translate api to v2

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -8,6 +8,7 @@ export default {
   PRIVACY_URL: 'https://iip-static-assets.s3.amazonaws.com/Commons/PrivacyPolicy.md',
   CONTACT_URL: 'https://iip-static-assets.s3.amazonaws.com/Commons/ContactUs.md',
   YOUTUBE_API_URL: 'https://www.googleapis.com/youtube/v3/videos',
+  GOOGLE_LANGUAGE_DETECT_URL: 'https://translation.googleapis.com/language/translate/v2/detect',
   EMAIL: 'design@america.gov',
   ERROR_MESSAGE: 'Oops! Something went wrong. If this issue persists, please email the IIP Office of Design',
   TIME_TO_STALE: 60000
--- client/src/containers/SearchBar/index.js
+++ client/src/containers/SearchBar/index.js
@@ -3,7 +3,7 @@
  * SearchBar
  *
  */
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -12,35 +12,81 @@ import * as actions from './actions';
 import makeSelectSearchBar from './selectors';
 import { Form } from 'semantic-ui-react';
 
+// Restructure to v2 layout - putting here to ensure it is ported from v1
+import axios from 'axios';
+import { languages, getDirection } from 'utils/language';
+import config from 'config';
+
 import './SearchBar.css';
 
-const SearchBar = ( props ) => {
-  const handleQueryOnChange = ( e ) => {
-    // this.props.updateSearchTerm( e.target.value );
-  };
+class SearchBar extends PureComponent {
+  constructor( props ) {
+    super( props );
+    this.URL = `${config.GOOGLE_LANGUAGE_DETECT_URL}?key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
+    this.state = {
+      // eslint-disable-next-line react/no-unused-state
+      direction: 'left'
+    };
+  }
 
-  const handleSubmit = ( e ) => {
+  getDetections = ( result ) => {
+    if ( result && result.data && result.data.data && result.data.data.detections ) {
+      const { detections } = result.data.data;
+      return detections.length && detections[0].length ? detections[0][0].language : null;
+    }
+    return null;
+  }
+
+  async fetchTextLanguage( text ) {
+    const result = await axios.post( `${this.URL}&q=${text}` );
+    const language = this.getDetections( result );
+    if ( language ) {
+      this.setState( {
+        // eslint-disable-next-line react/no-unused-state
+        direction: getDirection( language )
+      } );
+      return ( languages[language] ) ? languages[language] : null;
+    }
+    return null;
+  }
+
+  handleQueryOnChange = async ( e ) => {
+    const text = e.target.value;
+    // this.props.updateSearchQuery( text );
+    this.fetchTextLanguage( text )
+      .then( ( language ) => {
+        if ( language ) {
+          // this.props.languageUpdate( language );
+        }
+      } )
+      .catch( err => console.log( err ) );
+  }
+
+  handleSubmit = ( e ) => {
     e.preventDefault();
     // this.props.createRequest();
     // this.props.history.push( '/search' );
   };
 
-  return (
-    <section className="search_bar">
-      <Form onSubmit={ handleSubmit }>
-        <Form.Group>
-          <Form.Input
-            placeholder="Type in keywords to search our content"
-            className="search_input"
-            onChange={ handleQueryOnChange }
-            value={ props.searchTerm ? props.searchTerm : '' }
-          />
-          <Form.Button icon="search" type="submit" />
-        </Form.Group>
-      </Form>
-    </section>
-  );
-};
+  render() {
+    // This is not a complete port og google translate api; refer to v1 to complete
+    return (
+      <section className="search_bar">
+        <Form onSubmit={ this.handleSubmit }>
+          <Form.Group>
+            <Form.Input
+              placeholder="Type in keywords to search our content"
+              className="search_input"
+              onChange={ this.handleQueryOnChange }
+              value={ this.props.searchTerm ? this.props.searchTerm : '' }
+            />
+            <Form.Button icon="search" type="submit" />
+          </Form.Group>
+        </Form>
+      </section>
+    );
+  }
+}
 
 SearchBar.propTypes = {
   searchTerm: PropTypes.string
