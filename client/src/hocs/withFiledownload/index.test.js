041d5893f4f331c32ebb6e5460eeced4cdc618a4
Terri Regan <contact@terriregan.com>
2018-11-16 07:08:23 -0500

    Added filedownload hoc

---
+++ client/src/hocs/withFiledownload/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import withFiledownload from './index';
+
+// describe( '<withFiledownload />', () => {
+//   it( 'renders without crashing', () => {
+//     shallow( <withFiledownload /> );
+//   } );
+// } );
